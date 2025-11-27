fdbd6797dc0a25dbaa059327c0e5226d2b201858
ichimaru001 <ichimarusan345@proton.me>
2025-11-27 11:08:27 +0700

    feat: flesh out layout with detailed HTML structure

--- src/App.jsx
+++ src/App.jsx
@@ -10,81 +10,90 @@ function App() {
     <>
       <main>
           <header> // displays phone number, email, social media links
+            <div> // phone number, email
+              <div></div> // phone number
+              <div></div> // email
+            </div>
+            <div> // offer, social media links
+              <div></div> // offer
+              <div></div> // social media links
+            </div>
           </header>
+
           <nav> // stores navigation buttons, login/register, cart, favorites
-
+            <div></div> // Bookstar logo
+            <div></div> // nav buttons
+            <div></div> // login/register, cart, likes
           </nav>
-          <section> // breadcrumb navigation 
 
-          </section>
+          <section></section> // breadcrumb navigation 
+
           <section> // showing current product
             <figure> // main product image
-
+              <img src="" alt="" /> // product image
+              <span></span> // cycle through products
             </figure>
             <article> // main product description/buttons
-              <span> // book tags
-
-              </span>
+              <div> // book tags
+                <span></span> // single book tag
+              </div>
               <div> // main product descriptions
-
+                <h2></h2> // book title
+                <h3></h3> // book price
+                <p></p> // availability
+                <p></p> // book description
+                <div> // expanded book details
+                  <p></p> // pages
+                  <p></p> // publisher
+                  <p></p> // ISBN
+                  <p></p> // date published
+                </div>
               </div>
               <div> // user buttons, buy now, favorite, add to cart
-
+                <div>
+                  <button></button>    // buy now
+                </div>
+                <div>
+                  <button></button> // like
+                  <button></button> // cart
+                  <button></button> // watch list
+                </div>
               </div>
             </article>
           </section>
+
           <section> // reading list
-            <h2> // "Your Reading List" header
-
-            </h2>
+            <h2></h2> // "Your Reading List" header
             <article> // small  4 total
               <figure> // small product image
-
+                <img src="" alt="" />
               </figure>
               <div>
-                <h3> // book title
-
-                </h3>
-                <p> // book genre
-
-                </p>
+                <h3></h3> // book title
+                <p></p> // book genre
                 <div>
-                  <span> // initial price
-
-                  </span>
-                  <p> // final price
-
-                  </p>
+                  <span></span> // initial price
+                  <p></p> // final price
                 </div>
               </div>
             </article>
           </section>
+          
           <section> // books for you - two rows
-            <h2> // "Books For You" header
-
-            </h2>
+            <h2></h2> // "Books For You" header
             <article> // small  4 total
-              <figure> // small product image
-
-              </figure>
+              <figure></figure> // small product image
               <div>
-                <h3> // book title
-
-                </h3>
-                <p> // book genre
-
-                </p>
+                <h3></h3> // book title
+                <p></p> // book genre
                 <div>
-                  <span> // initial price
-
-                  </span>
-                  <p> // final price
-
-                  </p>
+                  <span></span> // initial price
+                  <p></p> // final price
                 </div>
               </div>
             </article>
           </section>
+
       </main>
     </>
   )
