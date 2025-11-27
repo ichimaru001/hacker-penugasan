9643454aa9b72403040d788dfc18fa93b51a76dc
ichimaru001 <ichimarusan345@proton.me>
2025-11-27 17:48:29 +0700

    feat(ui): implement layout and styling for current product carousel

--- src/components/CurrentProduct.jsx
+++ src/components/CurrentProduct.jsx
@@ -1,46 +1,178 @@
 import React from "react";
-
+import carouselImage from "../assets/carousel-inner.png";
 export const CurrentProduct = () => {
   return (
-    <section>
-      // showing current product
-      <figure>
-        // main product image
-        <img src="" alt="" /> // product image
-        <span></span> // cycle through products
-      </figure>
-      <article>
-        // main product description/buttons
-        <div>
-          // book tags
-          <span></span> // single book tag
-        </div>
-        <div>
-          // main product descriptions
-          <h2></h2> // book title
-          <h3></h3> // book price
-          <p></p> // availability
-          <p></p> // book description
-          <div>
-            // expanded book details
-            <p></p> // pages
-            <p></p> // publisher
-            <p></p> // ISBN
-            <p></p> // date published
+    <section
+      style={{
+        minHeight: "598px",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <div
+        style={{
+          minWidth: "506px",
+          minHeight: "475px",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          // columnGap: "1rem",
+          // flexWrap: "wrap",
+        }}
+      >
+        <figure>
+          <img src={carouselImage} alt="" />
+          {/* <span></span> */}
+        </figure>
+        <article
+          style={{
+            width: "506px",
+            height: "475px",
+            display: "flex",
+            flexDirection: "column",
+            // justifyContent: "center",
+            alignItems: "center",
+            // rowGap: "1rem",
+            flexWrap: "wrap",
+          }}
+        >
+          <div className="CurrentProduct-book-tag-div">
+            <span className="CurrentProduct-book-tag">Self Improvement</span>
+            <span className="CurrentProduct-book-tag">Technology</span>
           </div>
-        </div>
-        <div>
-          // user buttons, buy now, favorite, add to cart
           <div>
-            <button></button> // buy now
+            <h2
+              style={{
+                color: "#252B42",
+                fontSize: "2.4rem",
+                margin: "0",
+                marginTop: "1.4rem",
+              }}
+            >
+              Beyond the Stars
+            </h2>
+            <h3
+              style={{
+                color: "#252B42",
+                fontSize: "1.8rem",
+                margin: "0",
+              }}
+            >
+              $1,139.33
+            </h3>
+            <p>
+              <b>Availability :</b> In Stock
+            </p>
+            <p
+              style={{
+                wordWrap: "break-word",
+                margin: "0",
+              }}
+            >
+              Met minim Mollie non desert Alamo est sit cliquey dolor do met
+              sent. RELIT official consequent door ENIM RELIT Mollie. Excitation
+              venial consequent sent nostrum met. Author:Marcus pson
+            </p>
+            <div
+              style={{
+                marginTop: "0.9rem",
+              }}
+            >
+              <p className="CurrentProduct-book-details">
+                <b>Pages:</b> 328
+              </p>
+              <p className="CurrentProduct-book-details">
+                <b>Publisher:</b> Noir House Books
+              </p>
+              <p className="CurrentProduct-book-details">
+                <b>ISBN:</b> 978-1-234567-90-6
+              </p>
+              <p className="CurrentProduct-book-details">
+                <b>Published:</b> January 20, 2024
+              </p>
+            </div>
           </div>
-          <div>
-            <button></button> // like
-            <button></button> // cart
-            <button></button> // watch list
+          <div
+            style={{
+              width: "506px",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "flex-start",
+              // alignItems: "center",
+              columnGap: "1rem",
+              flexWrap: "wrap",
+              marginTop: "2rem",
+            }}
+          >
+            {/* // user buttons, buy now, favorite, add to cart */}
+            <div>
+              <button
+                style={{
+                  fontSize: "1rem",
+                  fontWeight: "500",
+                  width: "96px",
+                  height: "44px",
+                  backgroundColor: "#007AFF",
+                  border: "none",
+                  borderRadius: "12px",
+                }}
+              >
+                Buy Now
+              </button>
+              {/* // buy now */}
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "flex-start",
+                // alignItems: "center",
+                columnGap: "1rem",
+                // flexWrap: "wrap",
+                // marginTop: "2rem",
+              }}
+            >
+              <button
+                style={{
+                  width: "40px",
+                  height: "40px",
+                  backgroundColor: "#DBECFF",
+                  borderRadius: "100%",
+                  border: "none",
+                }}
+              >
+                H
+              </button>
+              {/* // like */}
+              <button
+                style={{
+                  width: "40px",
+                  height: "40px",
+                  backgroundColor: "#DBECFF",
+                  borderRadius: "100%",
+                  border: "none",
+                }}
+              >
+                C
+              </button>
+              {/* // cart */}
+              <button
+                style={{
+                  width: "40px",
+                  height: "40px",
+                  backgroundColor: "#DBECFF",
+                  borderRadius: "100%",
+                  border: "none",
+                }}
+              >
+                W
+              </button>
+              {/* // watch list */}
+            </div>
           </div>
-        </div>
-      </article>
+        </article>
+      </div>
     </section>
   );
 };
