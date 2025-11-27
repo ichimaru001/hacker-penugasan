9845c0759b29cfef25dc221234a02a24d4c694a9
ichimaru001 <ichimarusan345@proton.me>
2025-11-27 13:49:01 +0700

    feat(ui): implement layout and styling for navigation section

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,11 +2,41 @@ import React from "react";
 
 export const Navigation = () => {
   return (
-    <nav>
-      // stores navigation buttons, login/register, cart, favorites
-      <div></div> // Bookstar logo
-      <div></div> // nav buttons
-      <div></div> // login/register, cart, likes
+    <nav
+      style={{
+        backgroundColor: "#ffffffff",
+        width: "100vw",
+        minHeight: "96px",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        columnGap: "6rem",
+        // rowGap: "1rem",
+        flexWrap: "wrap",
+      }}
+    >
+      <h1 style={{ color: "#252B42" }}>Bookstar</h1>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          columnGap: "1rem",
+          flexWrap: "wrap",
+        }}
+      >
+        <button className="navigation-button">Home</button>
+        <button className="navigation-button">Shop</button>
+        <button className="navigation-button">About</button>
+        <button className="navigation-button">Blog</button>
+        <button className="navigation-button">Contact</button>
+        <button className="navigation-button">Pages</button>
+      </div>
+      <div>
+        <button className="login-register-button">Login / Register</button>
+      </div>
     </nav>
   );
 };
